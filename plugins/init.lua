07db7fa68da87668ef69964b9d94dcbed28737a3
Soliprem <franci.solidoro@gmail.com>
2022-10-07 23:16:49 +0200

    new

--- plugins/init.lua
+++ plugins/init.lua
@@ -28,5 +28,20 @@ return {
   ["akinsho/git-conflict.nvim"] = require "user.plugins.git-conflict",
   "davidgranstrom/nvim-markdown-preview",
   "ellisonleao/gruvbox.nvim",
-  ["vimwiki/vimwiki"] = require "user.plugins.vimwiki",
+  ["vimwiki/vimwiki"] = {
+    config = function()
+      vim.g.vimwiki_list = {
+        {
+          path = "~/vimwiki",
+          syntax = "markdown",
+          ext = ".md",
+          path_html = "~/.local/src/wiki/",
+          template_path = "~/.local/src/wiki/assets/layouts",
+          template_default = "default",
+          custom_wiki2html = "vimwiki_markdown",
+          template_ext = ".tpl",
+        },
+      }
+    end,
+  },
 }
